eb839f2735ec2d116dd49c2660a173837f39762d
Marc Lainez <marc.lainez@gmail.com>
2025-10-03 13:43:14 +0200

    Revert latest changes since they make the hat crash

--- lib/pololu_a_star32u4/driver.ex
+++ lib/pololu_a_star32u4/driver.ex
@@ -27,7 +27,6 @@ defmodule PololuAStar32u4.Driver do
   @i2c_addr 0x14  # A-Star 32U4 I2C slave address
   @i2c_bus "i2c-1"  # Default I2C bus on Raspberry Pi
   @i2c_delay 1  # Delay in milliseconds between I2C operations
-  @poll_interval 50  # ms, for button scanning
 
   @doc """
   Starts the Driver GenServer.
@@ -47,11 +46,6 @@ defmodule PololuAStar32u4.Driver do
   @doc false
   def read_buttons() do
     GenServer.call(__MODULE__, :read_buttons)
-  end
-
-  @doc false
-  def subscribe_buttons() do
-    GenServer.call(__MODULE__, {:subscribe, self()})
   end
 
   @doc false
@@ -80,12 +74,11 @@ defmodule PololuAStar32u4.Driver do
   end
 
   @impl true
-  def init(_) do
+  def init(state) do
     case I2C.open(@i2c_bus) do
       {:ok, ref} ->
         Logger.info("PololuAStar32u4.Driver started on bus #{@i2c_bus}")
-        :timer.send_interval(@poll_interval, :poll_buttons)
-        {:ok, %{ref: ref, buttons: %{a: false, b: false, c: false}, subscribers: [], song_task: nil, stop_flag: false}}
+        {:ok, Map.put(state, :ref, ref)}
       {:error, reason} ->
         Logger.error("Failed to open I2C bus #{@i2c_bus}: #{inspect(reason)}")
         {:stop, reason}
@@ -119,9 +112,18 @@ defmodule PololuAStar32u4.Driver do
     {:reply, values, state}
   end
 
-  def handle_call({:subscribe, pid}, _from, state) do
-    Process.monitor(pid)
-    {:reply, :ok, %{state | subscribers: [pid | state.subscribers]}}
+  def handle_call({:play_song, song}, _from, %{ref: ref} = state) do
+    chunks =
+      String.codepoints(song)
+      |> Enum.chunk_every(14)
+      |> Enum.map(&Enum.join/1)
+
+    Enum.each(chunks, fn chunk ->
+      play_notes(ref, chunk)
+      wait_until_done(ref)
+    end)
+
+    {:reply, :ok, state}
   end
 
   def handle_call(:read_encoders, _from, %{ref: ref} = state) do
@@ -129,85 +131,16 @@ defmodule PololuAStar32u4.Driver do
     {:reply, {l, r}, state}
   end
 
-  def handle_call(:stop_flag?, _from, %{stop_flag: flag} = state),
-    do: {:reply, flag, state}
-
-  @impl true
-  def handle_cast({:play_song, song}, %{ref: ref, song_task: nil} = state) do
-    task = Task.start(fn -> do_play_song(ref, song, self()) end) |> elem(1)
-    {:noreply, %{state | song_task: task, stop_flag: false}}
-  end
-
-  def handle_cast(:stop_song, %{ref: ref, song_task: task} = state) when not is_nil(task) do
-    Logger.info("Stopping song playback…")
-    payload = <<0>> <> String.duplicate(<<0>>, 14)
-    write(ref, 24, payload)
-    {:noreply, %{state | stop_flag: true}}
-  end
-
-  def handle_cast(:stop_song, state), do: {:noreply, state}
-
-  @impl true
-  def handle_info(:poll_buttons, %{ref: ref, buttons: last, subscribers: subs} = state) do
-    {:ok, <<a, b, c>>} = read(ref, 3, 3)
-    current = %{a: a == 1, b: b == 1, c: c == 1}
-
-    if current != last do
-      Enum.each(subs, fn pid -> send(pid, {:buttons_changed, current}) end)
-    end
-
-    {:noreply, %{state | buttons: current}}
-  end
-
-  def handle_info({:DOWN, _mref, :process, pid, _}, state) do
-    {:noreply, %{state | subscribers: List.delete(state.subscribers, pid)}}
-  end
-
-  def handle_info(:song_finished, state), do: {:noreply, %{state | song_task: nil, stop_flag: false}}
-
-
-  defp do_play_song(ref, song, parent) do
-    chunks =
-      String.codepoints(song)
-      |> Enum.chunk_every(14)
-      |> Enum.map(&Enum.join/1)
-
-    Enum.each(chunks, fn chunk ->
-      if GenServer.call(__MODULE__, :stop_flag?) do
-        {:halt, :stopped}
-      else
-        play_notes(ref, chunk)
-        wait_until_done(ref)
-        {:cont, :ok}
-      end
-    end)
-
-    send(parent, :song_finished)
-  end
-
   defp read(ref, offset, size) do
-    case I2C.write(ref, @i2c_addr, <<offset>>) do
-      :ok ->
-        Process.sleep(@i2c_delay)
-        I2C.read(ref, @i2c_addr, size)
-
-      {:error, reason} ->
-        Logger.error("I2C write failed at offset #{offset}: #{inspect(reason)}")
-        {:error, reason}
-    end
+    :ok = I2C.write(ref, @i2c_addr, <<offset>>)
+    Process.sleep(@i2c_delay)
+    I2C.read(ref, @i2c_addr, size)
   end
 
   defp write(ref, offset, payload) do
-    case I2C.write(ref, @i2c_addr, [offset | :binary.bin_to_list(payload)]) do
-      :ok ->
-        Process.sleep(@i2c_delay)
-        :ok
-
-      {:error, reason} ->
-        Logger.error("I2C write failed at offset #{offset}: #{inspect(reason)}")
-        emergency_stop(ref)
-        {:error, reason}
-    end
+    :ok = I2C.write(ref, @i2c_addr, [offset | :binary.bin_to_list(payload)])
+    Process.sleep(@i2c_delay)
+    :ok
   end
 
   defp play_notes(ref, notes) do
@@ -221,11 +154,6 @@ defmodule PololuAStar32u4.Driver do
       false -> :ok
       true -> wait_until_done(ref)
     end
-  end
-
-  defp emergency_stop(ref) do
-    I2C.write(ref, @i2c_addr, [6 | :binary.bin_to_list(<<0::little-signed-16, 0::little-signed-16>>)])
-    Logger.warning("Emergency motor stop issued due to I2C error")
   end
 
   defp read_flag(ref) do
